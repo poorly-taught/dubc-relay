c253966a4774e12f4121dfa4978fc67a9823138e
mcarter <mcarter@revolutioncompany.com>
2023-12-21 17:31:44 -0600

    initial gundb configuration

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import Gun from 'gun'
+import  { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+app.use(Gun.serve)
+app.use(express.static(__dirname))
+
+const server = app.listen(port)
+const gun = Gun({file: 'data', web: server})
+
+console.log('Server start on port ' + port + ' with /gun')
